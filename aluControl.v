c9fbf88429cff06c9a4a872d866ab84b57a34c04
ChanderG <chanderg@cse.iitkgp.ernet.in>
2014-10-27 15:40:48 +0530

    Simulator works again

--- aluControl.v
+++ aluControl.v
@@ -6,7 +6,7 @@ module aluControl_unit (ALUop, FuncCode, ALUCtrl);
     input [3:0] FuncCode;      // from instruction
     output[3:0] ALUCtrl;       //the original 4 bit instruction/order that OUR alu expects
     reg [3:0] ALUCtrl;         // so this module works closely with OUR alu
-always //@ (ALUCtrl or ALUop or FuncCode)
+always @ (ALUCtrl or ALUop or FuncCode)
 begin
 if(ALUop!=4'b1111)
     begin
